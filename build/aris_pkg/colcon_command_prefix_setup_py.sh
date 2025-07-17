3c0f2f4a6c306efef61affde188148bb6f9dd01f
hongha0704 <166518391+hongha0704@users.noreply.github.com>
2025-07-17 23:53:51 +0900

    Delete build directory

--- build/aris_pkg/colcon_command_prefix_setup_py.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/beakhongha/RobotArm/install/team4_msgs/share/team4_msgs/package.sh"
-. "/home/beakhongha/RobotArm/install/xArm-Python-SDK/share/xArm-Python-SDK/package.sh"
--- build/team4_msgs/rosidl_cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(team4_msgs_IDL_FILES "srv/PutOnIcecream.idl;msg/StoragyStatus.idl")
-set(team4_msgs_INTERFACE_FILES "srv/PutOnIcecream.srv;srv/PutOnIcecream_Request.msg;srv/PutOnIcecream_Response.msg;msg/StoragyStatus.msg")
